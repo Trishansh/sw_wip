82e17c497729e1186026a627f9ead3af1af94882
arjunmahishi <arjun.mahishi@gmail.com>
2017-02-25 11:32:20 +0530

    Added jquery, template and firebase

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDqogOUnUGzJqSWSdmAFD3XuzR8fB6lUvQ",
+    authDomain: "instifytest-a209b.firebaseapp.com",
+    databaseURL: "https://instifytest-a209b.firebaseio.com",
+    storageBucket: "instifytest-a209b.appspot.com",
+    messagingSenderId: "739385873928"
+  };
+  firebase.initializeApp(config);
